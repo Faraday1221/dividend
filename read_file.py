cb0fccd518fefbb5b83327060684e682d3f3d384
Faraday1221 <faraday1221@gmail.com>
2015-11-26 21:54:08 -0500

    added basic date handling examples

--- read_file.py
+++ read_file.py
@@ -6,7 +6,12 @@ Created on Mon Nov 23 23:54:56 2015
 """
 import os
 import csv
+from datetime import datetime
 
+
+#==============================================================================
+# Importing the data
+#==============================================================================
 # change to the directory where the csv is stored (assuming it is not part of
 # the path)
 file_dir = '/Users/jonbruno/Documents/Python/dividend'
@@ -27,5 +32,32 @@ with open(filename, 'rb') as source:
     file_in = csv.reader(source, delimiter="\t")
     for line in file_in:
         print line
-        
+
+#==============================================================================
+# Working with Dates
+#==============================================================================
 # we might want to think about grabbing the dates and converting their format
+# a nice time tutorial is here:
+# https://pymotw.com/2/datetime/
+
+# lets use an example line from Breitburn.txt
+x = ['2/1/2007', 'Cash', '0.399', '1/22/2007', '2/5/2007', '2/14/2007']
+
+date_object = datetime.strptime(x[0], '%m/%d/%Y')
+
+type(date_object) # datetime produces an object
+print(date_object) # this prints the raw datetime.datetime object
+print(datetime.date(date_object)) # this prints only the date (not time) part of the object
+print(date_object.strftime('%B %d, %Y')) # this is some formatting on the object
+
+# we may want date instead of datetime since we dont need the hour min sec args
+
+# we can look at the 
+t1 = datetime.strptime(x[0], '%m/%d/%Y')
+t2 = datetime.strptime(x[3], '%m/%d/%Y')
+print t1-t2 # this produces a timedelta in days which can be added to other dates
+
+#==============================================================================
+# Now... What od we want to look at - how do we need to structure our data?
+#==============================================================================
+# this is a question for David
